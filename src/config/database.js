d36ae9a836d27a0cd06e17f7524deca33d134dd6
“archit-classplus” <archit.bhatnagar@classplus.co>
2026-05-08 14:55:16 +0530

    added logs

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connectDB() {
   const uri = process.env.MONGODB_URI || 'mongodb://preprod_rw_user:StrongPreprodPassword%40123@10.16.17.40:27017/speak-app?authSource=speak-app';
-
+  console.log('uri', uri);
   mongoose.connection.on('disconnected', () => {
     console.warn('[DB] MongoDB disconnected');
   });
